1e044ac8d1ce6a684e95e15b9615534409accd82
unnamedddddddd <deniskamaldinov85@gmail.com>
2025-09-15 20:11:53 +0500

    Поменял что-то

--- ThirdWork/project3new.cpp
+++ ThirdWork/project3new.cpp
@@ -6,6 +6,25 @@
 
 using namespace std;
 
+vector<vector<int>> splitArray(const vector<int>& arr, size_t n) {
+    vector<vector<int>> result;
+    
+    if (n > arr.size()) n = arr.size();
+   
+    float base_size = arr.size() / n;
+    float remainder = arr.size() % n;
+    size_t start_index = 0;
+    
+    for (int i = 0; i < n; ++i) {
+        size_t current_size = base_size + (i < remainder ? 1 : 0);
+        vector<int> part(arr.begin() + start_index, arr.begin() + start_index + current_size);
+        result.push_back(part);
+        start_index += current_size;
+    }
+    
+    return result;
+}
+
 void File(int res_arr[]) {
 	ofstream input("data.txt");
 
@@ -22,61 +41,66 @@ void File(int res_arr[]) {
 	}
 }
 
-void TwoArrays(int arr1[], int arr2[]) {
+void TwoArrays(vector<int> arr1, vector<int> arr2) {
 	auto start = chrono::high_resolution_clock::now();
-	int* res_arr{ new int[100] {} };
 
-	for (int i = 0; i < 100; ++i)
+	vector<int> res_arr;
+	res_arr.reserve(arr1.size());
+
+	for (int i = 0; i < arr1.size(); ++i)
 	{
-		res_arr[i] = arr1[i] + arr2[i];
+		res_arr.push_back((arr1[i]) * (arr2[i]));
 	}
 
 	auto end = chrono::high_resolution_clock::now();
 	auto res_time = chrono::duration_cast<chrono::milliseconds>(end - start);
-	cout << res_arr[10] << " time: " << res_time.count() << " ms" << endl;
+
+	for (auto num : res_arr)
+	{
+		cout << num << " ";
+		if (num == 10)
+		{
+			break;
+		}
+	}
+	
+	cout << " time: " << res_time.count() << " ms" << endl;
 }
 
 int main() {
-
 	int num_threads;
-	cout << "Введите число потоков(от 1 - 4)";
+	size_t size_array;
+	cout << "Введите размер массива: ";
+	while (!(cin >> size_array)) {
+		cin.clear();
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cerr << "Ошибка: Неверный формат. Введите число: ";
+	}
+
+	cout << "Введите число потоков(от 1 - 4): ";
 	while (!(cin >> num_threads) || (num_threads < 1 || num_threads > 4)) {
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		cerr << "Ошибка: Неверный формат. Введите число: ";
 	}
-		
-	int* arr1 = nullptr;
-	int* arr2 = nullptr;
-	arr1 = new int[100]();	
-	arr2 = new int[100]();
 
+	vector<int> arr(size_array);
 	for (int i = 0; i < 100; ++i)
 	{
-		arr1[i] += i;
-		arr2[i] += i;
+		arr[i] = i + 1;
 	}
+
+	auto parts = splitArray(arr, size_array);
 	vector<thread> threads;
 	threads.reserve(num_threads);
 
 	for (int i = 0; i < num_threads; ++i)
 	{
-		threads.emplace_back(TwoArrays, arr1, arr2);
+		threads.emplace_back(TwoArrays, arr, arr);
 	}
-
-	// thread th1(TwoArrays, arr1, arr2);
-	// thread th2(TwoArrays, arr1, arr2);
-	// thread th3(TwoArrays, arr1, arr2);
-	// thread th4(TwoArrays, arr1, arr2);
 
 	for (auto& thread : threads) {
 		thread.join();
 	}
-	delete[] arr1;
-	delete[] arr2;
-	// th1.join();
-	// th2.join();
-	// th3.join();
-	// th4.join();
-
+	
 }
