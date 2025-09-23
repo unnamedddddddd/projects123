adb180ee710cefa2c3f12214a4f66850689889ed
Denis <deniskamaldinov85@gmail.com>
2025-09-23 21:12:45 +0500

    Дописал чуть чуть 5 работу

--- FifthWork/projectOOP3.cpp
+++ FifthWork/projectOOP3.cpp
@@ -23,8 +23,7 @@ class Agency : public Product
 	string m_End;
 	float m_Budget = 0;
 	json m_j;
-
-public:
+ public:
 	Agency(string FIO, string Place, string Start, string End, float Budget){
 		m_j["name"] = FIO;
 		m_j["place"] = Place;
@@ -44,7 +43,7 @@ public:
 			cout << "Файл не был открыт\n";
 		}
 	}
-
+	
 	void OutputFile() {
 
 		ifstream out("data.json");
@@ -54,19 +53,19 @@ public:
 			if (m_j.is_object())
 			{
 				if (m_j.contains("name") && m_j["name"].is_string()) {
-					cout << "Имя: " << m_j["name"].get<string>() << endl;
+					cout << u8"Имя: " << m_j["name"].get<string>() << endl;
 				}
 				if (m_j.contains("place") && m_j["place"].is_string()) {
-					cout << "Place: " << m_j["place"].get<string>() << endl;
+					cout << u8"Место: " << m_j["place"].get<string>() << endl;
 				}
 				if (m_j.contains("start") && m_j["start"].is_string()) {
-					cout << "Start: " << m_j["start"].get<string>() << endl;
+					cout << u8"Начало поездки: " << m_j["start"].get<string>() << endl;
 				}
 				if (m_j.contains("end") && m_j["end"].is_string()) {
-					cout << "End: " << m_j["end"].get<string>() << endl;
+					cout << u8"Конец поездки: " << m_j["end"].get<string>() << endl;
 				}
 				if (m_j.contains("budget") && m_j["budget"].is_number_float()) {
-					cout << "Budget: " << m_j["budget"].get<float>() << endl;
+					cout << u8"Бюджет: " << m_j["budget"].get<float>() << endl << endl;
 				}
 			}
 			out.close();
@@ -75,40 +74,48 @@ public:
 		{
 			cout << "Файл не был открыт\n";
 		}
-
-
-
-
 	}
 };
 
 int main()
 {
-	locale::global(locale(""));
-	SetConsoleCP(CP_UTF8);
-	SetConsoleOutputCP(CP_UTF8);
+	try {
+		setlocale(LC_ALL, "ru_RU.UTF-8");
+		SetConsoleCP(CP_UTF8);
+		SetConsoleOutputCP(CP_UTF8);
 
-	/*
-	ifstream out("data.json");
-	if (out.is_open())
-	{
-		j = json::parse(out);
-		if (j.is_object())
-		{
-            if (j.contains("name") && j["name"].is_string()) {
-				cout << "Имя: " << j["name"].get<string>() << endl;
-            }
-			if (j.contains("age") && j["age"].is_number_integer()) {
-				cout << "Age: " << j["age"].get<int>() << endl;
+		int choice = 1;
+		do {
+			
+			string name, place, start, end;
+			float budget = 0.0;
+			cout << u8"Введите ФИО: ";  getline(cin, name);
+			cout << u8"Место поездки: "; getline(cin, place);
+			cout << u8"Начало поездки: ";  getline(cin, start);
+			cout << u8"Конец поездки: ";  getline(cin, end);
+			cout << u8"Бюджет: "; cin >> budget;
+			cout << endl;
+			if (cin.fail()) {
+				cout << "Ошибка: не тот тип" << endl;
+				cin.clear();
+				continue;
 			}
-		}
-		out.close();*/
+
+			Agency agent(name, place, start, end, budget);
+			agent.InputFile();
+			agent.OutputFile();
+
+			cout << u8"Продолжить?(1 - да, 0 - нет)" << endl;
+			cin >> choice;
+			cin.clear();
+			cin.ignore(32767, '\n');
+
+		} while (choice != 0);
+		return 0;
+	}
+	catch(exception &ex){
+		cerr << ex.what() << endl;
 	
-
-	Agency agent("Denis папап ","Russia","21.09.2025","27.09.2025",23324.4);
-	agent.InputFile();
-	agent.OutputFile();
-
-
-	return 0;
+	}
+	
 }
